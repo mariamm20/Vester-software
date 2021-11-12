63d3366f4b2e8d939f4b3a7357ea0621a7bc1fa8
unknown <mero5691@gmail.com>
2021-11-13 01:54:46 +0200

    friday update

--- admin/js/main.js
+++ admin/js/main.js
@@ -6,3 +6,36 @@
         new bootstrap.Tooltip(tooltipTriggerEl)
     })
 })()
+
+
+function appear(){
+    let x = document.querySelector(".pickColor");
+    if (x.style.display == "block")
+    x.style.display = "none";
+    else
+        x.style.display = "block";
+}
+let lis = document.querySelectorAll(".pickColor ul li");
+let exp = document.querySelector("header")
+let btn = document.querySelector(".fix")
+
+if(localStorage.getItem("color")){
+    exp.style.backgroundColor = localStorage.getItem("color");
+    btn.style.backgroundColor = localStorage.getItem("color");
+    lis.forEach((li) =>{
+        li.classList.remove("active");
+    });
+    // document.querySelector(`[data-color="${localStorage.getItem("color")}]`).classList.add("active");
+}
+
+lis.forEach((li) =>{
+    li.addEventListener("click",(e) => {
+        lis.forEach((li) => {
+            li.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
+        localStorage.setItem("color",e.currentTarget.dataset.color);
+        exp.style.backgroundColor = e.currentTarget.dataset.color;
+        btn.style.backgroundColor = e.currentTarget.dataset.color;
+    })
+})
