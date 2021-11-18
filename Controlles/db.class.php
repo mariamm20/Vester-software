bba9dc0469e9992d216477f648a3068a4b314601
MOHAMEDHESMAM8 <59764426+MOHAMEDHESMAM8@users.noreply.github.com>
2021-11-18 23:02:04 +0200

    update db

--- Controlles/db.class.php
+++ Controlles/db.class.php
@@ -5,10 +5,10 @@ class Db {
     private $host ="localhost";
     private $user = "root" ;
     private $pwd ="";
-    private $dbname ="Vester_Software" ;
-
+    private $dbname ="vester_software" ;
+    
     protected function  Connect(){
-        $dns = 'mysql:host='.$this->host.';dbname'.$this->$dbname;
+        $dns = 'mysql:host='.$this->host.';dbname='.$this->dbname;
         $pdo = new PDO($dns,$this->user,$this->pwd);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
         return $pdo ;
