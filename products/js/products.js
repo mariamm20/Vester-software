f98f1f33162345c943422a27b58809db0df2b536
unknown <mero5691@gmail.com>
2021-11-21 01:09:47 +0200

    grid update

--- products/js/products.js
+++ products/js/products.js
@@ -34,14 +34,42 @@ function menu() {
 
 }
 
-function appear(){
+function appear() {
     let x = document.querySelector(".list");
-    if (x.style.display == "none"){
+
+    if (x.style.display == "none") {
         x.style.display = "block";
+
     }
-    else{
+    else {
         x.style.display = "none";
+
     }
-    
+
 }
+
+function change1() {
+    let x = document.querySelector(".a");
+    x.classList.toggle("rotate")
+
+
+}
+function change2() {
+    let x = document.querySelector(".b");
+    x.classList.toggle("rotate")
+
+}
+function change3() {
+    let x = document.querySelector(".c");
+    x.classList.toggle("rotate")
+
+}
+
+
+
+
+
+
+
+
 
