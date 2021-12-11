7f2e6500acdb162c13c3c44eed7ed53b37df7eac
unknown <mero5691@gmail.com>
2021-12-11 08:30:08 +0200

    country

--- sign-up/js/sign-up.js
+++ sign-up/js/sign-up.js
@@ -34,3 +34,42 @@ function menu() {
 
 }
 
+
+
+//api countries
+
+fetch('https://restcountries.com/v2/all').then(res =>{
+    return res.json();
+}).then(data =>{
+    let output= '<option>Select Country</option>';
+    
+    data.forEach(data =>{
+         output += `<option value=${data.name}> ${data.name} (+${data.callingCodes})</option>`
+        // console.log(`<img width="10" height="10" src="${data.flag}" />`)
+    })
+    document.getElementById("countries").innerHTML = output;
+    
+}).catch(err =>{
+    console.log(err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
