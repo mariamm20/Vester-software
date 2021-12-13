27647e68d312f2c6a13a255725005eee8c7061a0
mariamm20 <59734637+mariamm20@users.noreply.github.com>
2021-12-13 10:18:51 +0200

    Add files via upload

---
+++ home/js/setting.js
@@ -0,0 +1,57 @@
+let section = document.getElementById("menu");
+let bar = document.querySelector(".fa-bars");
+let header = document.querySelector("header");
+let cart = document.querySelector(".cart");
+let logo = document.querySelector(".v a");
+
+function menu() {
+    section.classList.add("smooth");
+    if (section.style.display == "none") {
+        section.style.display = "flex";
+        let s = window.innerHeight - 75;
+        section.style.height = `${s}px`;
+        bar.classList.remove("fa-bars");
+        bar.classList.add("fa-times");
+        document.body.style.overflowY = "hidden";
+        header.style.backgroundColor = "#1c2750"
+        header.style.color = "#fff";
+        cart.style.color = "#1c2750";
+        logo.style.color = "#fff";
+        document.body.style.padding = "0px";
+
+    }
+    else {
+        section.style.display = "none";
+        bar.classList.remove("fa-times");
+        bar.classList.add("fa-bars");
+        document.body.style.overflow = "visible";
+        header.style.backgroundColor = "#fff";
+        header.style.color = "#1c2750";
+        logo.style.color = "#1c2750";
+        document.body.style.padding = "5px";
+
+    }
+
+}
+
+
+function myFunction() {
+    var x = document.querySelector(".pass1");
+    var y = document.querySelector(".pass2")
+    if (x.type === "password" && y.type === "password") {
+        x.type = "text";
+        y.type = "text";
+    } else {
+        x.type = "password";
+        y.type  = "password";
+    }
+}
+
+
+
+
+
+
+
+
+
