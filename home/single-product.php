1b8fd518058765f88c82a26f182f3b19497e5cef
mariamm20 <59734637+mariamm20@users.noreply.github.com>
2021-12-24 03:01:36 +0200

    Update single-product.php

--- home/single-product.php
+++ home/single-product.php
@@ -374,19 +374,10 @@
 
 
 
-    <footer>
-        <div class="footer">
-            <h6 style="letter-spacing: 4px;">CONTACT US</h6>
-
-            <div class="icons">
-                <i class="fas fa-envelope"></i>
-                <i class="fab fa-facebook-f "></i>
-                <i class="fab fa-twitter "></i>
-                <i class="fab fa-instagram"></i>
-            </div>
-        </div>
-        <span>&copy;All Rights Reserved 2021-2022</span>
-    </footer>
+<!--footer-->
+    <?php
+    include "../includes/footer.inc.php";
+    ?>
 
 
 
