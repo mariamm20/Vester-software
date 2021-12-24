316f94e799391f080444e1719a3145c60d269c2e
mariamm20 <59734637+mariamm20@users.noreply.github.com>
2021-12-24 07:49:12 +0200

    Update setting.js

--- home/js/setting.js
+++ home/js/setting.js
@@ -11,6 +11,38 @@ function myFunction() {
         y.type  = "password";
     }
 }
+var output = document.getElementById("images");
+let showoneimg = function () {
+
+    var input = document.getElementById("img");
+    if(output.style.display == "none"){
+        output.style.display == "block"
+    }
+    
+    output.style.display = "block";
+    if (output.innerHTML == "") {
+        for (let i = 0; i < 1; i++) {
+            output.innerHTML += '<li>' + input.files.item(i).name + '</li>';
+        }
+    } else {
+        output.innerHTML = "";
+        for (let i = 0; i < 1; i++) {
+            output.innerHTML += '<li>' + input.files.item(i).name + '</li>';
+        }
+    }
+
+}
+var btn1 = document.querySelector(".change-list .btn1");
+btn1.addEventListener("click", function(){
+output.innerHTML="";
+output.style.display="none";
+})
+var btn2 = document.querySelector(".change-list .btn2");
+btn2.addEventListener("click", function () {
+    output.innerHTML = "";
+    output.style.display = "none";
+})
+
 
 
 
